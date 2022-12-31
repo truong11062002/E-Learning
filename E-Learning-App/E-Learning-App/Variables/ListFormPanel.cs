77cd77ffac3a8c6e49f2f0e6f4c95434673b1186
nn_truong1106 <truong11062002@gmail.com>
2022-12-31 09:20:03 +0700

    commit template

---
+++ E-Learning-App/E-Learning-App/Variables/ListFormPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace E_Learning_App.Variables
+{
+    class ListFormPanel
+    {
+        private static List<Panel> listFormsPanel = new List<Panel>();
+
+        public static List<Panel> ListFormsPanel { get => listFormsPanel; set => listFormsPanel = value; }
+    }
+}
